b98a8922eeda3b7299133a3128c7a8c5cb8eac30
Franco Polizzi <me@fpolizzi.com>
2026-07-24 20:56:54 +0200

    feat: solve leetcode 169 - Majority Element

---
+++ src/main/java/com/fpolizzi/MajorityElement.java
@@ -0,0 +1,32 @@
+package com.fpolizzi;
+
+/**
+ * Created by fpolizzi on 7/24/26
+ */
+public class MajorityElement {
+
+    static void main() {
+
+        int[] nums = {2, 2, 1, 3, 1, 2, 2};
+
+        System.out.println((majorityElement(nums)));
+    }
+
+    public static int majorityElement(int[] nums) {
+
+        int majority = nums[0], counts = 1;
+
+        for (int i = 1; i < nums.length; i++) {
+            if (counts == 0) {
+                counts++;
+                majority = nums[i];
+            } else if (majority == nums[i]) {
+                counts++;
+            } else {
+                counts--;
+            }
+        }
+
+        return majority;
+    }
+}
