1536afb00ebfafa79d8abcb875cbec92787e0e24
Franco Polizzi <me@fpolizzi.com>
2026-07-27 20:54:07 +0200

    feat: solve leetcode 242 - Valid Anagram

---
+++ src/main/java/com/fpolizzi/ValidAnagram.java
@@ -0,0 +1,41 @@
+package com.fpolizzi;
+
+/**
+ * Created by fpolizzi on 7/27/26
+ */
+public class ValidAnagram {
+
+    static void main() {
+
+        String s = "anagram";
+        String t = "nagaram";
+
+        System.out.println((validAnagram(s, t)));
+    }
+
+    public static boolean validAnagram(String s, String t) {
+
+        s = s.toLowerCase();
+        t = t.toLowerCase();
+
+        s = s.replace(" ", "");
+        t = t.replace(" ", "");
+
+        int[] counts = new int[26];
+
+        for (int i = 0; i < s.length(); i++) {
+            counts[s.charAt(i) - 'a']++;
+        }
+
+        for (int i = 0; i < t.length(); i++) {
+            counts[t.charAt(i) - 'a']--;
+        }
+
+        for(int count : counts) {
+            if (count != 0)
+                return false;
+        }
+
+        return true;
+    }
+}
