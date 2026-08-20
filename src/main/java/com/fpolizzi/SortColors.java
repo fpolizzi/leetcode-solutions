9448255eba8f4e2ae02dcef926d03641e735eb77
Franco Polizzi <me@fpolizzi.com>
2026-08-20 23:33:31 +0200

    feat: solve leetcode 75 - Sort Colors

---
+++ src/main/java/com/fpolizzi/SortColors.java
@@ -0,0 +1,43 @@
+package com.fpolizzi;
+
+/**
+ * Created by fpolizzi on 8/20/26
+ */
+public class SortColors {
+
+    public void sortColors(int[] nums) {
+
+        int start = 0;
+        int mid = 0;
+        int end = nums.length - 1;
+
+        while (mid <= end) {
+
+            switch (nums[mid]) {
+                case 0:
+                    // swap with start index
+                    swap(nums, start, mid);
+                    mid++;
+                    start++;
+                    break;
+
+                case 1:
+                    mid++;
+                    break;
+
+                case 2:
+                    // swap with end index
+                    swap(nums, mid, end);
+                    end--;
+                    break;
+            }
+        }
+    }
+
+    private void swap(int[] arr, int pos1, int pos2) {
+
+        int temp = arr[pos1];
+        arr[pos1] = arr[pos2];
+        arr[pos2] = temp;
+    }
+}
