ae0376d129152a520cae2074d885187a271e48cb
Franco Polizzi <me@fpolizzi.com>
2026-09-04 21:59:05 +0200

    feat: solve leetcode 079 - Word Search

---
+++ src/main/java/com/fpolizzi/WordSearch.java
@@ -0,0 +1,46 @@
+package com.fpolizzi;
+
+/**
+ * Created by fpolizzi on 9/4/26
+ */
+public class WordSearch {
+
+    private int rows, cols;
+    private boolean[][] visited;
+
+    public boolean exist(char[][] board, String word) {
+        rows = board.length;
+        cols = board[0].length;
+        visited = new boolean[rows][cols];
+
+        for (int r = 0; r < rows; r++) {
+            for (int c = 0; c < cols; c++) {
+                if (dfs(board, word, r, c, 0)) {
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    private boolean dfs(char[][] board, String word, int r, int c, int i) {
+
+        if (i == word.length()) {
+            return true;
+        }
+
+        if (r < 0 || c < 0 || r >= rows || c >= cols ||
+                board[r][c] != word.charAt(i) || visited[r][c]) {
+            return false;
+        }
+
+        visited[r][c] = true;
+        boolean res = dfs(board, word, r + 1, c, i + 1) ||
+                dfs(board, word, r - 1, c, i + 1) ||
+                dfs(board, word, r, c + 1, i + 1) ||
+                dfs(board, word, r, c - 1, i + 1);
+        visited[r][c] = false;
+
+        return res;
+    }
+}
