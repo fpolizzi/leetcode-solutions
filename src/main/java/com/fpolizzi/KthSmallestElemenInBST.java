c1bf9fe95f4563956ead53bf781308982970c0d9
Franco Polizzi <me@fpolizzi.com>
2026-09-07 20:38:30 +0200

    feat: solve leetcode 210 - Kth Smallest Element In a Bst

---
+++ src/main/java/com/fpolizzi/KthSmallestElemenInBST.java
@@ -0,0 +1,61 @@
+package com.fpolizzi;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by fpolizzi on 9/7/26
+ */
+public class KthSmallestElemenInBST {
+
+    public int kthSmallest(TreeNode root, int k) {
+        List<Integer> arr = new ArrayList<>();
+
+        dfs(root, arr);
+        return arr.get(k - 1);
+    }
+
+    private void dfs(TreeNode node, List<Integer> arr) {
+        if (node == null) {
+            return;
+        }
+
+        dfs(node.left, arr);
+        arr.add(node.val);
+        dfs(node.right, arr);
+    }
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
