0e741043c3166f77e553c45fa05d1ce63e7451c3
Franco Polizzi <me@fpolizzi.com>
2026-09-11 20:03:39 +0200

    feat: solve leetcode 105 - Construct Binary Tree From Preorder And Inorder Traversal

---
+++ src/main/java/com/fpolizzi/ConstructBinaryTreeFromPreorderAndInorderTraversal.java
@@ -0,0 +1,72 @@
+package com.fpolizzi;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * Created by fpolizzi on 9/11/26
+ */
+public class ConstructBinaryTreeFromPreorderAndInorderTraversal {
+
+    public TreeNode buildTree(int[] preOrder, int[] inOrder) {
+
+        Map<Integer, Integer> inOrderIndexMap = new HashMap<>();
+        for (int i = 0; i < inOrder.length; i++)
+            inOrderIndexMap.put(inOrder[i], i);
+
+        return splitTree(preOrder, inOrderIndexMap,
+                0, 0, inOrder.length - 1);
+    }
+
+    TreeNode splitTree(int[] preOrder, Map<Integer, Integer> inOrderIndexMap,
+                       int rootIndex, int left, int right) {
+
+        TreeNode root = new TreeNode(preOrder[rootIndex]);
+
+        // Create left and right subtree
+        int mid = inOrderIndexMap.get(preOrder[rootIndex]);
+        if (mid > left)
+            root.left = splitTree(preOrder, inOrderIndexMap,
+                    rootIndex + 1, left, mid - 1);
+        if (mid < right)
+            root.right = splitTree(preOrder, inOrderIndexMap,
+                    rootIndex + mid - left + 1, mid + 1, right);
+        return root;
+    }
+
+    /**
+     * Definition for a binary tree node.
+     * public class TreeNode {
+     * int val;
+     * TreeNode left;
+     * TreeNode right;
+     * TreeNode() {}
+     * TreeNode(int val) { this.val = val; }
+     * TreeNode(int val, TreeNode left, TreeNode right) {
+     * this.val = val;
+     * this.left = left;
+     * this.right = right;
+     * }
+     * }
+     */
+    class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+}
+
+
